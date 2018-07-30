8517d7bd7842d07386f7f4bf729673c8a1833af1
Kendall Sanford <kdesigns702@kendalls-imac.local>
2018-07-30 16:21:41 -0700

    Initial Commit

---
+++ Test App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test App
+//
+//  Created by Kendall Sanford on 7/30/18.
+//  Copyright © 2018 Kendall Sanford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
