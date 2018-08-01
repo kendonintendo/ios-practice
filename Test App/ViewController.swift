6e8140ca3bbb44a9829ab6e2d860af153f35887a
Kendall Sanford <ksanford711@gmail.com>
2018-07-31 23:28:19 -0700

    IOS Blank Setup

--- Test App/ViewController.swift
+++ Test App/ViewController.swift
@@ -13,13 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createLabel();
+        //buildTriangle();
+    }
+    
+    func createLabel(){
+        _ = UIView();
+        let labelRect = CGRect(x: 20, y: 20, width: 100, height: 100)
+        let label = UILabel(frame: labelRect)
+        label.text = "Kendall is the best at writting code and you know it! lol this is some more code."
+        label.font = UIFont(name: "Helvetica", size: 12.0)
+        label.textAlignment = .center
+        view.addSubview(label)
+        label.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        label.numberOfLines = 5
+    }
+    
+    func buildTriangle(){
+        
+        let path =  UIBezierPath();
+        path.move(to: CGPoint(x: 80, y: 50))
+        path.addLine(to: CGPoint(x: 140,y:150))
+        path.addLine(to: CGPoint(x: 10, y: 150))
+        path.close()
+        
+        UIColor.green.setFill()
+        UIColor.black.setStroke()
+        path.fill()
+        path.stroke()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
